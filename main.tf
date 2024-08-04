f20df08cda3414819b4b12d8aacc051dc8d7354e
Bruno Oliveira <brunoserrasilva@gmail.com>
2024-08-04 14:26:38 -0300

    Explorando o terraform show e destruindo recursos

--- main.tf
+++ main.tf
@@ -17,9 +17,16 @@ provider "aws" {
 resource "aws_instance" "bia_local" {
   ami = "ami-02f3f602d23f1659d"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0b12d1fb347fd4462"]
+
+  root_block_device {
+    volume_size = 10
+  }
+  
 
   tags = {
     ambiente = "dev"
     Name = "bia-local"
   }
+
 }
